646dbf0fa56d73c97558b14adc601a0d8b5d9f9b
Dean <gao.dean@hotmail.com>
2022-10-20 20:58:21 +1100

    feat: implement filters ui

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-
-function addListeners() {
-  document.getElementsByName('filter-day').forEach((node) => {
-    console.log(node);
-    node.addEventListener('click', function (e) {
-      if (this.getAttribute('checked') === 'true') {
-        this.className = 'filter-day outline';
-        this.setAttribute('checked', 'false');
-      } else {
-        this.className = 'filter-day';
-        this.setAttribute('checked', 'true');
-      }
-    });
+function addToggleFunctionality(node, classes) {
+  node.addEventListener('click', function _() {
+    if (this.getAttribute('checked') === 'true') {
+      this.className = `${classes} outline`;
+      this.setAttribute('checked', 'false');
+    } else {
+      this.className = classes;
+      this.setAttribute('checked', 'true');
+    }
   });
 }
 
-addListeners();
+document.getElementsByName('filter-day')
+  .forEach((node) => addToggleFunctionality(node, node.name));
+document.getElementsByName('filter-time')
+  .forEach((node) => addToggleFunctionality(node, node.name));
