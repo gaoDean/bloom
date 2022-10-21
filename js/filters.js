1a2e734eeb4186baaec31ea8bd3a16616bd86524
Dean <gao.dean@hotmail.com>
2022-10-21 21:51:13 +1100

    feat: display when filter not default

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,4 @@
+const filterElement = document.getElementById('filters');
 const filters = {
   days: {
     name: 'Days...',
@@ -20,26 +21,48 @@ const filters = {
   },
 };
 
-const filterElement = document.getElementById('filters');
+function parseFilterUI(filterListNode) {
+  let list = [];
+  Array.from(filterListNode.getElementsByTagName('li')).forEach((label) => {
+    label = label.firstElementChild;
+    let name = label.textContent;
+    let checked = label.firstElementChild.checked;
+    list.push([name, checked]);
+  });
+  return list;
+}
+
 function addFilter(key, value) {
   let list = '';
-  Object.values(value.list).forEach((label) => {
+  let defaultList = Object.values(value.list);
+  defaultList.forEach((label) => {
     const checked = label[1] ? 'checked' : '';
     list = `${list}<li><label><input type='checkbox' ${checked}>${label[0]}</label></li>\n`;
   });
 
-  const fullHTML = `
+  const newFilterHTML = `
         <div class='filter'>
           <details role='list' class='filter-container filter-${key}-container'>
-            <summary class='filter-time' aria-haspopup='listbox'>${value.name}</summary>
+            <summary class='filter-${key}' aria-haspopup='listbox'>${value.name}</summary>
             <ul role='listbox'>
               ${list}
             </ul>
           </details>
         </div>`;
 
-  filterElement.insertAdjacentHTML('beforeend', fullHTML);
+  filterElement.insertAdjacentHTML('beforeend', newFilterHTML);
+  console.log(filterElement.lastElementChild.firstElementChild);
+  filterElement.lastElementChild.firstElementChild.addEventListener('click', function _(node) {
+    let parsedList = parseFilterUI(this.lastElementChild);
+    if (JSON.stringify(defaultList) !== JSON.stringify(parsedList)) {
+      this.firstElementChild.className = `filter-${key} filter-changed`;
+    } else {
+      this.firstElementChild.className = `filter-${key}`;
+    }
+  });
 }
 
 Object.entries(filters).forEach(([key, value]) => addFilter(key, value));
+
+// flex space between uses margin-right, this cancels it for the rightmost element
 filterElement.lastElementChild.style.cssText = 'margin-right: 0';
