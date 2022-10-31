3d4a3ccdadc3b49990bfbb3f2ee3629071641c9a
Dean <gao.dean@hotmail.com>
2022-10-31 21:28:23 +1100

    feat: custom pico scss and more steamlined filter ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+import '$lib/css/pico/custom.scss';
+</script>
+
 <svelte:head>
 	<title>Job search</title>
 </svelte:head>
@@ -8,7 +12,5 @@
 :root {
 	--primary: #3fb8be !important;
 	--primary-hover: #3aacb2 !important;
-	--filter-option-height: 2rem;
-	--filter-option-border-radius: calc(var(--filter-option-height) * 0.6);
 }
 </style>
